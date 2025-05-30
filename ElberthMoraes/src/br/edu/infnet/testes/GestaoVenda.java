5121e8ab723aa502ef018dc397b8526d4e8fad4d
Elberth Lins Costa de Moraes <elberth.moraes@dataprev.gov.br>
2025-05-30 10:01:18 -0300

    Atualização do projeto para o paradigma OO

---
+++ ElberthMoraes/src/br/edu/infnet/testes/GestaoVenda.java
@@ -0,0 +1,125 @@
+package br.edu.infnet.testes;
+
+import java.util.Scanner;
+
+public class GestaoVenda {
+
+	private static Venda[] vendas;
+	private static float valorTotalVendas;		
+	private static Scanner leitura;
+	private static int qtdeVenda;
+	
+	private static boolean categoriaValida(int categoria){
+		return categoria < 1 || categoria > 5;
+	}
+
+	private static void cadastrarVenda(){
+		if(qtdeVenda >= Constante.QTDE_MAX_VENDAS) {
+			System.err.println("Limite alcançado! Impossível realizar novas vendas!");
+			return;
+		}
+
+		String descricao = null;
+		float preco = 0;	
+		int categoria = 0;
+		boolean desconto = false;
+		int quantidade = 0;
+						
+		System.out.println("Venda #" + (qtdeVenda+1));
+		
+		System.out.print("Entre com a descrição do produto: ");
+		descricao = leitura.next();
+
+		System.out.print("Entre com a quantidade do produto: ");
+		quantidade = leitura.nextInt();
+
+		System.out.print("Entre com o preço do produto: ");
+		preco = leitura.nextFloat();
+
+		System.out.print("Indique se o produto tem desconto (true/false): ");
+		desconto = leitura.nextBoolean();
+		
+		System.out.println("1 - Alimentação");
+		System.out.println("2 - Limpeza");
+		System.out.println("3 - Vestuário");
+		System.out.println("4 - Papelaria");
+		System.out.println("5 - Brinquedo");
+		System.out.print("Escolha a categoria do produto: ");
+		categoria = leitura.nextInt();
+		
+		if(categoriaValida(categoria)) {
+			System.err.println("Categoria inválida: "+categoria+"!!");
+		} else {
+
+			Venda venda = new Venda(descricao, preco, categoria);
+			venda.desconto = desconto;
+			venda.quantidade = quantidade;
+					
+			vendas[qtdeVenda] = venda;
+		
+			valorTotalVendas = valorTotalVendas + venda.calcularValorVenda();
+		}			
+		
+		qtdeVenda++;		
+	}
+	
+	private static void impressaoVendas(){
+
+		for(int j = 0; j < qtdeVenda; j++) {
+			System.out.println(vendas[j]);
+		}
+		
+		System.out.printf("O valor total de vendas realizadas é R$%.2f.", valorTotalVendas);		
+	}
+
+	private static int obterMenu(){
+		System.out.println("Menu de opções:");
+		System.out.println("1 - Lançar venda");
+		System.out.println("2 - Imprimir vendas");
+		System.out.println("9 - Sair");
+		System.out.printf("Escolha a opção desejada: ");
+
+		return leitura.nextInt();
+	}
+	
+	public static void main(String[] args) {
+
+		if(args.length != 1) {
+			System.err.println("Entrada inválida: não conseguimos identificar nenhum valor!");
+			return;
+		}
+		
+		leitura = new Scanner(System.in);
+		
+		vendas = new Venda[Constante.QTDE_MAX_VENDAS];		
+
+		int opcao = 1;
+		
+		do {
+			
+			opcao = obterMenu();
+			
+			switch (opcao) {
+			case Constante.CADASTRAR:				
+				cadastrarVenda();
+				break;
+
+			case Constante.IMPRIMIR:
+				impressaoVendas();
+				break;
+				
+			case Constante.SAIR:
+				System.out.println("Preparação para saída!");
+				break;
+
+			default:
+				System.out.println("Opção inválida! Tente novamente!");
+				break;
+			}
+			
+		} while (opcao != 9);
+
+		leitura.close();
+	}
+
+}
--- ElberthMoraes/src/br/edu/infnet/testes/Venda.java
+++ ElberthMoraes/src/br/edu/infnet/testes/Venda.java
@@ -2,16 +2,56 @@ package br.edu.infnet.testes;
 
 public class Venda {
 
-	String descricao;
+	private String descricao;
 	int quantidade;
-	float preco;
+	private float preco;
 	boolean desconto;
-	int categoria;
-	float valorVenda;	
+	private int categoria;
 
 	public Venda(String descricao, float preco, int categoria) {
+		
+		this.preco = atualizarPrecoPorCategoria(categoria, preco);
+				
 		this.descricao = descricao;
-		this.preco = preco;
 		this.categoria = categoria;
+	}
+	
+	private float atualizarPrecoPorCategoria(int categoria, float preco){
+		switch (categoria) {
+		case 1:
+			return preco * 1.05f;
+
+		case 2:
+			return preco * 1.08f;
+
+		case 3:
+			return preco * 1.10f;
+
+		case 4:
+		case 5:
+			return preco * 1.06f;
+		}
+
+		return 0;
+	}
+	
+	float calcularValorVenda(){
+
+		float valorVenda = 0;	
+
+		valorVenda = this.quantidade * this.preco;					
+		if(desconto) {
+			valorVenda = valorVenda * Constante.VALOR_DESCONTO;
+		}
+
+		return valorVenda;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return String.format("Produto (%s): %s; qtde: %d; R$ %.2f; desconto: %s\nTotal da venda: %.2f\n\n",
+				categoria, descricao, quantidade, preco, desconto, calcularValorVenda()
+			);
 	}
 }
